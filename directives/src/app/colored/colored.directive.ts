8edc9f92545c565bf3fa7c7a6dc3e75cd0c7413f
isabella-projects <damyan4oy@gmail.com>
2024-05-22 17:54:48 +0200

    Some styling changes

--- directives/src/app/colored/colored.directive.ts
+++ directives/src/app/colored/colored.directive.ts
@@ -14,8 +14,11 @@ import {
 export class ColoredDirective implements OnInit {
     @Input() defaultColor: string = 'transparent';
     @Input() highlightColor: string = 'blue';
+    @Input() defaultPadding: string;
+    @Input() hoverPadding: string;
 
     @HostBinding('style.backgroundColor') backgroundColor: string;
+    @HostBinding('style.padding') padding: string;
     constructor(
         private element: ElementRef,
         private renderer: Renderer2,
@@ -24,15 +27,18 @@ export class ColoredDirective implements OnInit {
     ngOnInit(): void {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
         this.backgroundColor = this.defaultColor;
+        this.padding = this.defaultPadding;
     }
 
     @HostListener('mouseenter') mouseover(eventData: Event) {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
         this.backgroundColor = this.highlightColor;
+        this.padding = this.hoverPadding;
     }
 
     @HostListener('mouseleave') mouseleave(eventData: Event) {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
         this.backgroundColor = this.defaultColor;
+        this.padding = this.defaultPadding;
     }
 }
