33f382299523fb6ad866db9a988c2b9ec50bf3f2
isabella-projects <damyan4oy@gmail.com>
2024-05-27 15:21:07 +0200

    Migrated the app to module-based approach

--- NEW_VERSION/essentials/src/app/app.component.ts
+++ NEW_VERSION/essentials/src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-
 import { type User } from './user/user.model';
 
 import { DUMMY_USERS } from './users';
 
 @Component({
     selector: 'app-root',
-    standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
     users: User[] = DUMMY_USERS;
--- NEW_VERSION/essentials/src/app/user/user.component.ts
+++ NEW_VERSION/essentials/src/app/user/user.component.ts
@@ -7,16 +7,12 @@ import {
     // input,
 } from '@angular/core';
 
-import { CardComponent } from '../shared/card/card.component';
-
 import { type User } from './user.model';
 
 @Component({
     selector: 'app-user',
-    standalone: true,
     templateUrl: './user.component.html',
     styleUrl: './user.component.css',
-    imports: [CardComponent],
 })
 export class UserComponent {
     @Input({ required: true }) user!: User;
