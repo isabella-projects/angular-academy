3e8772501486be614904d5f21c461b20769012f8
isabella-projects <damyan4oy@gmail.com>
2024-06-26 17:06:27 +0200

    Added custom directive for leaving the site with query params

--- NEW_VERSION/directives/src/app/app.component.ts
+++ NEW_VERSION/directives/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [AuthComponent, LearningResourcesComponent],
+    imports: [AuthComponent, LearningResourcesComponent, NgIf],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+    private authService = inject(AuthService);
+    isAdmin = computed(() => this.authService.activePermission() === 'admin');
+}
