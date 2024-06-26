973f1f1a9d95deb29e72602b2e8d6552addc1140
isabella-projects <damyan4oy@gmail.com>
2024-06-26 17:30:25 +0200

    Built structural directive for rendering specific elements

--- NEW_VERSION/directives/src/app/app.component.ts
+++ NEW_VERSION/directives/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component, computed, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+
 import { AuthService } from './auth/auth.service';
-import { NgIf } from '@angular/common';
+
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [AuthComponent, LearningResourcesComponent, NgIf],
+    imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
