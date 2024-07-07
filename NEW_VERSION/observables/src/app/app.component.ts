058d0b320158ece2cc505cfb79c887901da089aa
isabella-projects <damyan4oy@gmail.com>
2024-07-07 15:20:17 +0200

    Implemented custom observable which emits new value - practicing

--- NEW_VERSION/observables/src/app/app.component.ts
+++ NEW_VERSION/observables/src/app/app.component.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -18,16 +18,33 @@ import { interval, map } from 'rxjs';
     imports: [],
 })
 export class AppComponent implements OnInit {
+    /* Practicing 
     clickCount = signal(0);
     clickCount$ = toObservable(this.clickCount);
 
     interval$ = interval(1000);
-    interval = toSignal(this.interval$, { initialValue: 0 });
+    intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+    */
+
+    customInterval$ = new Observable((subscriber) => {
+        let timesExecuted = 0;
+
+        const interval = setInterval(() => {
+            if (timesExecuted > 3) {
+                clearInterval(interval);
+                subscriber.complete();
+                return;
+            }
+
+            subscriber.next({ message: 'New value', value: 1 });
+            timesExecuted++;
+        }, 2000);
+    });
 
     private destroyRef = inject(DestroyRef);
 
     constructor() {
-        /* Using Angular effect method
+        /* Practicing - using Angular effect method
         effect(() => {
             console.log(`Clicked button ${this.clickCount()} times.`);
         });
@@ -35,6 +52,12 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.customInterval$.subscribe({
+            next: (val) => console.log(val),
+            complete: () => console.log('Completed'),
+        });
+
+        /* Practicing (we can also pass object and use next(), error(), completed() methods)
         const subscription = this.clickCount$.subscribe((_val) => {
             console.log(`Clicked button ${this.clickCount()} times.`);
         });
@@ -42,8 +65,8 @@ export class AppComponent implements OnInit {
         this.destroyRef.onDestroy(() => {
             subscription.unsubscribe();
         });
-
-        /* Example of RxJS observables
+        */
+        /* Example of RxJS observables and pipes for modifications
         const subscription = interval(1000)
             .pipe(map((val) => val * 2))
             .subscribe((val) => {
@@ -57,6 +80,8 @@ export class AppComponent implements OnInit {
     }
 
     onClick(): void {
+        /* Practicing
         this.clickCount.update((prevCount) => prevCount + 1);
+        */
     }
 }
