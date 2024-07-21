ca84bd001990b8beeaa2c1eb553f6cc9a15cff67
isabella-projects <damyan4oy@gmail.com>
2024-07-21 21:19:12 +0200

    Reactive Forms - better way of handling forms
    Finished with template driven approach and started working with Reactive Forms. Handled form submission with them & added validators.

--- 12-forms/src/app/app.component.ts
+++ 12-forms/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './auth/login/login.component';
+
+import { FormType } from './auth/login/login.model';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [LoginComponent],
+    imports: [LoginComponent, FormsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+    formType: FormType = 'reactive';
+}
--- 12-forms/src/app/auth/login/login.component.ts
+++ 12-forms/src/app/auth/login/login.component.ts
@@ -1,57 +1,100 @@
 import {
-    afterNextRender,
     Component,
     DestroyRef,
+    effect,
     inject,
+    input,
     viewChild,
 } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+    FormControl,
+    FormGroup,
+    FormsModule,
+    NgForm,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 import { debounceTime } from 'rxjs';
+
+import { FormType } from './login.model';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, ReactiveFormsModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
 })
 export class LoginComponent {
-    // Template Driven Approach
+    formType = input.required<FormType>();
 
-    private form = viewChild.required<NgForm>('form');
+    reactiveForm = new FormGroup({
+        email: new FormControl('', {
+            validators: [Validators.email, Validators.required],
+        }),
+        password: new FormControl('', {
+            validators: [Validators.minLength(6), Validators.required],
+        }),
+    });
+
+    private templateDrivenForm = viewChild.required<NgForm>('form');
     private destroyRef = inject(DestroyRef);
 
+    get isEmailValid() {
+        return (
+            this.reactiveForm.controls.email.touched &&
+            this.reactiveForm.controls.email.dirty &&
+            this.reactiveForm.controls.email.invalid
+        );
+    }
+
+    get isPassValid() {
+        return (
+            this.reactiveForm.controls.password.touched &&
+            this.reactiveForm.controls.password.dirty &&
+            this.reactiveForm.controls.password.invalid
+        );
+    }
+
     constructor() {
-        afterNextRender(() => {
-            const savedForm = localStorage.getItem('saved-login-form');
-
-            if (savedForm) {
-                const loadedFormData = JSON.parse(savedForm);
-                const savedEmail = loadedFormData.email;
-
-                setTimeout(() => {
-                    this.form().controls['email'].setValue(savedEmail);
-                }, 1);
+        effect(() => {
+            if (this.formType() === 'reactive') {
             }
 
-            const subscription = this.form()
-                .valueChanges?.pipe(debounceTime(500))
-                .subscribe({
-                    next: ({ email }) => {
-                        localStorage.setItem(
-                            'saved-login-form',
-                            JSON.stringify({ email }),
+            if (this.formType() === 'template') {
+                console.log('template');
+
+                const savedForm = localStorage.getItem('saved-login-form');
+
+                if (savedForm) {
+                    const loadedFormData = JSON.parse(savedForm);
+                    const savedEmail = loadedFormData.email;
+
+                    setTimeout(() => {
+                        this.templateDrivenForm().controls['email'].setValue(
+                            savedEmail,
                         );
-                    },
-                });
-            this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+                    }, 1);
+                }
+
+                const subscription = this.templateDrivenForm()
+                    .valueChanges?.pipe(debounceTime(500))
+                    .subscribe({
+                        next: ({ email }) => {
+                            localStorage.setItem(
+                                'saved-login-form',
+                                JSON.stringify({ email }),
+                            );
+                        },
+                    });
+                this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+            }
         });
     }
 
-    onSubmit(formData: NgForm): void {
-        if (formData.form.invalid) {
-            return;
-        }
+    onSubmitTD(formData: NgForm): void {
+        if (formData.form.invalid) return;
+
         const enteredEmail = formData.form.value.email;
         const enteredPassword = formData.form.value.password;
 
@@ -59,5 +102,9 @@ export class LoginComponent {
         console.log(enteredEmail, enteredPassword);
 
         formData.form.reset();
+    }
+
+    onSubmitRF() {
+        console.log(this.reactiveForm);
     }
 }
