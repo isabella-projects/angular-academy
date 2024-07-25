193ea1c2f547752e55408679d7900f0e500a6a44
isabella-projects <damyan4oy@gmail.com>
2024-07-25 18:14:29 +0200

    Signup form now fully connected
    Connected inputs for a complex form, nested form groups etc.

--- 12-forms/src/app/auth/signup/signup.component.ts
+++ 12-forms/src/app/auth/signup/signup.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {
+    FormArray,
     FormControl,
     FormGroup,
     ReactiveFormsModule,
     Validators,
 } from '@angular/forms';
+import { MemberType } from './signup.model';
 
 @Component({
     selector: 'app-signup',
@@ -18,8 +20,39 @@ export class SignupComponent {
         email: new FormControl('', {
             validators: [Validators.email, Validators.required],
         }),
-        password: new FormControl('', {
-            validators: [Validators.required, Validators.minLength(6)],
+        passwords: new FormGroup({
+            password: new FormControl('', {
+                validators: [Validators.required, Validators.minLength(6)],
+            }),
+            confirmPassword: new FormControl('', {
+                validators: [Validators.required, Validators.minLength(6)],
+            }),
+        }),
+
+        firstName: new FormControl('', { validators: [Validators.required] }),
+        lastName: new FormControl('', { validators: [Validators.required] }),
+
+        address: new FormGroup({
+            street: new FormControl('', { validators: [Validators.required] }),
+            number: new FormControl('', { validators: [Validators.required] }),
+            postalCode: new FormControl('', {
+                validators: [Validators.required],
+            }),
+            city: new FormControl('', { validators: [Validators.required] }),
+        }),
+
+        role: new FormControl<MemberType>('student', {
+            validators: [Validators.required],
+        }),
+
+        source: new FormArray([
+            new FormControl(false),
+            new FormControl(false),
+            new FormControl(false),
+        ]),
+
+        agree: new FormControl<boolean>(false, {
+            validators: [Validators.required],
         }),
     });
 
---
+++ 12-forms/src/app/auth/signup/signup.model.ts
@@ -0,0 +1,6 @@
+export type MemberType =
+    | 'student'
+    | 'teacher'
+    | 'employee'
+    | 'founder'
+    | 'other';
