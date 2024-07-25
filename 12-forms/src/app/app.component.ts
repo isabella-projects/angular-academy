2072e06ed7a8e05d22693db5331967d18472d102
isabella-projects <damyan4oy@gmail.com>
2024-07-25 17:46:50 +0200

    Handling the signup form
    Connected to the template with ReactiveFormsModule

--- 12-forms/src/app/app.component.ts
+++ 12-forms/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [LoginComponent, FormsModule],
+    imports: [LoginComponent, SignupComponent, FormsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
