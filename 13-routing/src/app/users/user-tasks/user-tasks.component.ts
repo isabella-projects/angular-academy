cc7ea89e35c7c22a915b2fb5f2cf5c1989b0e8cf
isabella-projects <damyan4oy@gmail.com>
2024-07-26 18:39:22 +0200

    Implemented dynamic routing for user tasks
    Fixed a bug where selected user highlight is applied on all users.
    Having commented two different approaches by using - Angular Signals and by using ActivatedRoute instance

--- 13-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 13-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    computed,
+    inject,
+    input,
+    // Input,
+    // DestroyRef,
+    // OnInit,
+} from '@angular/core';
+
+import { UsersService } from '../users.service';
+
+// import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-user-tasks',
@@ -7,4 +19,48 @@ import { Component } from '@angular/core';
     templateUrl: './user-tasks.component.html',
     styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+    /* Most common approach by having the same property name as the route 'userId' in this case */
+    userId = input.required<string>();
+
+    private usersService = inject(UsersService);
+
+    userName = computed(
+        () =>
+            this.usersService.users.find((user) => user.id === this.userId())
+                ?.name
+    );
+
+    /* Another old approach by using observables - same property as the route name is NOT needed
+    userName = '';
+
+    constructor(
+        private usersService: UsersService,
+        private activatedRoute: ActivatedRoute,
+        private destroyRef: DestroyRef
+    ) {}
+    // or injecting it directly in the constructor
+
+    ngOnInit(): void {
+        console.log(this.activatedRoute);
+        const subscription = this.activatedRoute.paramMap.subscribe({
+            next: (paramMap) => {
+                this.userName =
+                    this.usersService.users.find(
+                        (user) => user.id === paramMap.get('userId')
+                    )?.name || '';
+            },
+        });
+        this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    }
+    */
+
+    /* Getting username without signals
+    @Input({ required: true }) userId!: string;
+
+    get userName() {
+        return this.usersService.users.find((user) => user.id === this.userId)
+            ?.name;
+    }
+    */
+}
