d40dd33766016bf8014afe64358cafa44d8c3c0f
isabella-projects <damyan4oy@gmail.com>
2024-07-26 17:29:13 +0200

    Setting up project for exploring angular router

---
+++ 13-routing/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [HeaderComponent, UsersComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+})
+export class AppComponent {}
---
+++ 13-routing/src/app/tasks/task/task.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject, input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CardComponent } from '../../shared/card/card.component';
+
+import { TasksService } from '../tasks.service';
+
+import { type Task } from './task.model';
+
+@Component({
+    selector: 'app-task',
+    standalone: true,
+    imports: [DatePipe, CardComponent],
+    templateUrl: './task.component.html',
+    styleUrl: './task.component.css',
+})
+export class TaskComponent {
+    task = input.required<Task>();
+
+    private tasksService = inject(TasksService);
+
+    onComplete() {
+        this.tasksService.removeTask(this.task().id);
+    }
+}
