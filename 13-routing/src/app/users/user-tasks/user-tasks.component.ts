b05f0207ff09424a00204db8739fd03cfe2e62be
isabella-projects <damyan4oy@gmail.com>
2024-07-27 14:47:19 +0200

    Added child routes for user tasks

--- 13-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 13-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -9,13 +9,14 @@ import {
 } from '@angular/core';
 
 import { UsersService } from '../users.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 // import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-user-tasks',
     standalone: true,
-    imports: [],
+    imports: [RouterOutlet, RouterLink],
     templateUrl: './user-tasks.component.html',
     styleUrl: './user-tasks.component.css',
 })
