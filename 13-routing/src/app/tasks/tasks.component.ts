662f62b6a1fbb2bfbd6aadcd5e6ee2d426d2cfdd
isabella-projects <damyan4oy@gmail.com>
2024-07-31 13:05:18 +0200

    Implemented route-based lazy loading

--- 13-routing/src/app/tasks/tasks.component.ts
+++ 13-routing/src/app/tasks/tasks.component.ts
@@ -1,14 +1,8 @@
-import { Component, inject, input } from '@angular/core';
-import {
-    ActivatedRouteSnapshot,
-    ResolveFn,
-    RouterLink,
-    RouterStateSnapshot,
-} from '@angular/router';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
 
-import { TasksService } from './tasks.service';
 import { Task, TaskOrder } from './task/task.model';
 
 @Component({
@@ -23,25 +17,3 @@ export class TasksComponent {
     userId = input.required<string>();
     order = input<TaskOrder>();
 }
-
-// Resolver function to get all user tasks - cleaner way
-export const resolveUserTasks: ResolveFn<Task[]> = (
-    activatedRoute: ActivatedRouteSnapshot,
-    routerState: RouterStateSnapshot,
-) => {
-    const order = activatedRoute.queryParams['order'];
-    const tasksService = inject(TasksService);
-    const tasks = tasksService
-        .allTasks()
-        .filter(
-            (task) => task.userId === activatedRoute.paramMap.get('userId'),
-        );
-
-    if (order && order === 'asc') {
-        tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-    } else {
-        tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-    }
-
-    return tasks.length ? tasks : [];
-};
--- 13-routing/src/app/users/users.routes.ts
+++ 13-routing/src/app/users/users.routes.ts
@@ -1,19 +1,50 @@
-import { Routes } from '@angular/router';
-import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { inject } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    ResolveFn,
+    RouterStateSnapshot,
+    Routes,
+} from '@angular/router';
 import {
     canLeaveEditPage,
     NewTaskComponent,
 } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+
+const resolveUserTasks: ResolveFn<Task[]> = (
+    activatedRoute: ActivatedRouteSnapshot,
+    routerState: RouterStateSnapshot,
+) => {
+    const order = activatedRoute.queryParams['order'];
+    const tasksService = inject(TasksService);
+    const tasks = tasksService
+        .allTasks()
+        .filter(
+            (task) => task.userId === activatedRoute.paramMap.get('userId'),
+        );
+
+    if (order && order === 'asc') {
+        tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+    } else {
+        tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+    }
+
+    return tasks.length ? tasks : [];
+};
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'tasks',
-        pathMatch: 'prefix', // in this case we could also use 'prefix'
+        pathMatch: 'full', // in this case we could also use 'prefix'
     },
     {
         path: 'tasks', // <domain>/users/<id>/tasks
-        component: TasksComponent,
+        loadComponent: () =>
+            import('../tasks/tasks.component').then(
+                (mod) => mod.TasksComponent,
+            ),
         runGuardsAndResolvers: 'always',
         resolve: {
             userTasks: resolveUserTasks,
