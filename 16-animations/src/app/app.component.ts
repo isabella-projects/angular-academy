199a4d9bef7dea3bb8cca31f5185aa1e35812636
isabella-projects <damyan4oy@gmail.com>
2024-08-02 17:57:28 +0200

    Implemented advanced transitions & having transition state changes
    Use @provideAnimationAsync in Angular 18 for animations which are not triggered on app load! Otherwise use eagerly loaded module provider @provideAnimation

--- 16-animations/src/app/app.component.ts
+++ 16-animations/src/app/app.component.ts
@@ -30,22 +30,67 @@ import {
                     transform: 'translateX(100px)',
                 }),
             ),
+            transition('normal <=> highlighted', animate(500)),
+        ]),
+        trigger('wildState', [
+            state(
+                'normal',
+                style({
+                    backgroundColor: 'red',
+                    transform: 'translateX(0) scale(1)',
+                }),
+            ),
+            state(
+                'highlighted',
+                style({
+                    backgroundColor: 'blue',
+                    transform: 'translateX(100px) scale(1)',
+                }),
+            ),
+            state(
+                'shrunken',
+                style({
+                    backgroundColor: 'green',
+                    transform: 'translateX(0) scale(0.5)',
+                }),
+            ),
             transition('normal => highlighted', animate(300)),
             transition('highlighted => normal', animate(800)),
+            transition('shrunken <=> *', [
+                style({
+                    backgroundColor: 'orange',
+                    borderRadius: 0,
+                }),
+                animate(
+                    1000,
+                    style({
+                        borderRadius: '50px',
+                    }),
+                ),
+                animate(500),
+            ]),
         ]),
     ],
 })
 export class AppComponent {
     state: string = 'normal';
+    wildState: string = 'normal';
+
     list: string[] = ['Milk', 'Sugar', 'Bread'];
 
     onAnimate(): void {
         this.state == 'normal'
             ? (this.state = 'highlighted')
             : (this.state = 'normal');
+
+        this.wildState == 'normal'
+            ? (this.wildState = 'highlighted')
+            : (this.wildState = 'normal');
     }
 
-    onShrink(): void {}
+    onShrink(): void {
+        this.wildState = 'shrunken';
+    }
 
     onAdd(item: string): void {
         this.list.push(item);
