8d45bddd010f82aeacd9acf1bb0392595cd95aac
isabella-projects <damyan4oy@gmail.com>
2024-08-02 14:40:23 +0200

    Initialized animations project & tested provideAnimationsAsync provider
    Added some basic styling
    Added trigger [@divState] bound to 'state' property

---
+++ 16-animations/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+import { state, style, trigger } from '@angular/animations';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    animations: [
+        trigger('divState', [
+            state(
+                'normal',
+                style({
+                    backgroundColor: 'red',
+                    transform: 'translateX(0)',
+                }),
+            ),
+            state(
+                'highlighted',
+                style({
+                    backgroundColor: 'blue',
+                    transform: 'translateX(100px)',
+                }),
+            ),
+        ]),
+    ],
+})
+export class AppComponent {
+    state: string = 'normal';
+    list: string[] = ['Milk', 'Sugar', 'Bread'];
+
+    onAnimate(): void {
+        this.state == 'normal'
+            ? (this.state = 'highlighted')
+            : (this.state = 'normal');
+    }
+
+    onShrink(): void {}
+
+    onAdd(item: string): void {
+        this.list.push(item);
+    }
+
+    onDelete(item: string): void {
+        this.list.splice(this.list.indexOf(item), 1);
+    }
+}
