2b1db37adb758eccb3859b86ce525d2ef17b45bc
isabella-projects <damyan4oy@gmail.com>
2024-08-03 11:47:26 +0200

    Using keyframes and grouping animations

--- 16-animations/src/app/app.component.ts
+++ 16-animations/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import {
     animate,
+    group,
+    keyframes,
     state,
     style,
     transition,
@@ -70,6 +72,7 @@ import {
                 animate(500),
             ]),
         ]),
+        // First list in the template
         trigger('list1', [
             state(
                 'in',
@@ -93,6 +96,60 @@ import {
                         transform: 'translateX(100px)',
                     }),
                 ),
+            ]),
+        ]),
+        // Second list in the template
+        trigger('list2', [
+            state(
+                'in',
+                style({
+                    opacity: 1,
+                    transform: 'translateX(0)',
+                }),
+            ),
+            transition('void => *', [
+                animate(
+                    1000,
+                    keyframes([
+                        style({
+                            transform: 'translateX(-100px)',
+                            opacity: 0,
+                            offset: 0,
+                        }),
+                        style({
+                            transform: 'translateX(-50px)',
+                            opacity: 0.5,
+                            offset: 0.3,
+                        }),
+                        style({
+                            transform: 'translateX(-20px)',
+                            opacity: 1,
+                            offset: 0.8,
+                        }),
+                        style({
+                            transform: 'translateX(0px)',
+                            opacity: 1,
+                            offset: 1,
+                        }),
+                    ]),
+                ),
+            ]),
+            transition('* => void', [
+                group([
+                    animate(
+                        300,
+                        style({
+                            color: 'red',
+                        }),
+                    ),
+                    animate(
+                        800,
+                        style({
+                            opacity: 0,
+                            transform: 'translateX(100px)',
+                        }),
+                    ),
+                ]),
             ]),
         ]),
     ],
